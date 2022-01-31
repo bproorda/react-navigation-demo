54493ff7f0e79c9353b77f56add7382817139c53
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2022-01-30 18:48:18 -0600

    moving between screens

--- App.js
+++ App.js
@@ -1,22 +1,30 @@
 // In App.js in a new project
 
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-function HomeScreen() {
+function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
     </View>
   );
 }
 
-function DetailsScreen() {
+function DetailsScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
+      <Button
+        title="Go back to Home"
+        onPress={() => navigation.goBack()} //navigation.popToTop() goes to the first screen in the stack, for going past multiple screens
+      />
     </View>
   );
 }
